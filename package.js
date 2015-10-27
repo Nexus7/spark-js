03272b1e1ca39f8350a14e88ea49453bc88a2da4
Nexus7 <rickbehl@hotmail.com>
2015-10-27 12:18:38 +0800

    Updated git repo

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: 'Provide a wrapper around the Particle (previously Spark) library to interact with hardware devices.',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/Nexus7/spark-js.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
