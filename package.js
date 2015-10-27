ec9da6fbf6223aba8089bec198838cb8ab97d355
Nexus7 <rickbehl@hotmail.com>
2015-10-27 12:15:22 +0800

    Initial commit

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+    name: 'rickbehl:spark-js',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'Provide a wrapper around the Particle (previously Spark) library to interact with hardware devices.',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Npm.depends({
+    'spark': '1.0.0'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
+    api.addFiles(['spark-js.js'], 'server');
+    api.export('Spark', 'server');
+});
+
+Package.onTest(function(api) {
+    api.use('tinytest');
+    api.use('ecmascript');
+    api.use('rickbehl:spark-js');
+    api.addFiles('rickbehl:spark-js-tests.js');
+});
